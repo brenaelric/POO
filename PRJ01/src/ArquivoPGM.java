00b0ad4a885181c20dc9e0191d300d24552ebe06
brenaelric <brenakesia6@gmail.com>
2017-05-30 22:38:04 -0300

    Vai sem tratamento por enquanto T_T

--- PRJ01/src/ArquivoPGM.java
+++ PRJ01/src/ArquivoPGM.java
@@ -1,5 +1,3 @@
-import java.io.DataInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Paths;
@@ -11,89 +9,17 @@ public class ArquivoPGM {
   private int largura;
   private int max;
   private int[][] imagem;// = new int [altura][largura];
-  
 
-  public int getAltura() {
-	return altura;
-}
-
-public void setAltura(int altura) {
-	this.altura = altura;
-}
-
-public int getLargura() {
-	return largura;
-}
-
-public void setLargura(int largura) {
-	this.largura = largura;
-}
-
-public int getMax() {
-	return max;
-}
-
-public void setMax(int max) {
-	this.max = max;
-}
-
-public int[][] getImagem() {
-	return imagem;
-}
-
-public void setImagem(int[][] imagem) {
-	this.imagem = imagem;
-}
-
-public void carregaPGM(String caminho) throws Exception {
+  public void carregaPGM(String caminho) throws Exception {
     Scanner s = new Scanner(Paths.get(caminho));
-    FileInputStream cArquivo = new FileInputStream(caminho);
-    DataInputStream arquivo = new DataInputStream(cArquivo);
-    
     s.nextLine();// P2
-    
-    int b = arquivo.read();
-    int count = 0;
-	while(count<altura*largura){
-    	if(b=='#'){
-    		arquivo.readLine();
-    		count++;
-    	}
-    	else{
-    		largura = s.nextInt();
-    		altura = s.nextInt(); 
-    		break;
-    	}    	
-    }
-	
+    largura = s.nextInt();
+    altura = s.nextInt();
     imagem = new int[this.largura][this.largura];
-    while(count<altura*largura){
-    	if(b=='#'){
-    		arquivo.readLine();
-    		count++;
-    	}
-    	else{
-    		max = s.nextInt(); // 255
-    		break;
-    	}    	
-    }
-    
+    max = s.nextInt(); // 255
     for (int i = 0; i < largura; i++) {
       for (int j = 0; j < altura; j++) {
-    	  if(b<0)
-    		  break;
-    	  if(b== '\n'){
-    		  //Não faz nada
-    		  }
-    	  else if(b == '#'){
-    		  arquivo.readLine();
-    		  }
-    	  else if(Character.isWhitespace(b)){
-    		  //Não faz nada
-    		  }
-    	  else{
-    		  imagem[i][j] = s.nextInt();  
-    	  }
+        imagem[i][j] = s.nextInt();
       }
     }
   }
--- PRJ01/src/Teste.java
+++ PRJ01/src/Teste.java
@@ -2,7 +2,7 @@
 public class Teste {
 	public static void main(String []args) throws Exception{
 		ArquivoPGM A = new ArquivoPGM();
-		A.carregaPGM("mona_lisa.ascii.pgm");
-		A.escrevePGM("mona_lisa2.ascii.pgm");
+		A.carregaPGM("baboon.ascii.pgm");
+		A.escrevePGM("baboon2.ascii.pgm");
 	}
 }
