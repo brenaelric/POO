a49874bb680ce866f051b9a5e3f5cef6f4cf4bd3
Brena Elric <brenaelric@users.noreply.github.com>
2017-06-27 11:10:58 -0300

    Update Calculadora.java

--- Operandos/src/Calculadora.java
+++ Operandos/src/Calculadora.java
@@ -2,16 +2,20 @@
 public class Calculadora {
 
 	public static void main(String[] args) {
-		Operando a = new Operando(1);
-		Operando b = new Operando(2);
-		OperacaoSoma s = new OperacaoSoma("Soma", "+");
-		OperacaoSubtracao r = new OperacaoSubtracao("Subtracao", "-");
-		OperacaoMultiplicacao x = new OperacaoMultiplicacao("Multiplicao", "*");
-		OperacaoDivisao y = new OperacaoDivisao("Subtracao", "/");
-		s.calculoResultado(a, b);
-		r.calculoResultado(a, b);
-		x.calculoResultado(a, b);
-		y.calculoResultado(a, b);
+		try{
+			Operando a = new Operando(1);
+			Operando b = new Operando(2);
+			OperacaoSoma s = new OperacaoSoma("Soma", "+");
+			OperacaoSubtracao r = new OperacaoSubtracao("Subtracao", "-");
+			OperacaoMultiplicacao x = new OperacaoMultiplicacao("Multiplicao", "*");
+			OperacaoDivisao y = new OperacaoDivisao("Subtracao", "/");
+			s.calculoResultado(a, b);
+			r.calculoResultado(a, b);
+			x.calculoResultado(a, b);
+			y.calculoResultado(a, b);
+		} catch(Runtime Exception e){
+			
+		}
 	}
 
 }
