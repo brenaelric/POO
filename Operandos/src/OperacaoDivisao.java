1cf1c512e4ba2e0acb378093a84d0060a9937b75
Brena Elric <brenaelric@users.noreply.github.com>
2017-06-27 11:00:05 -0300

    Update OperacaoDivisao.java

--- Operandos/src/OperacaoDivisao.java
+++ Operandos/src/OperacaoDivisao.java
@@ -11,25 +11,16 @@ public class OperacaoDivisao extends OperacaoBinaria{
 		super(nome, simboloOperador);
 	}
 	
-	public float calculoResultado(Operando ... valor) {
+	public float calculoResultado(Operando ... valor) throws NumeroOperandosException, DivisaoPorZeroException{
 		if(valor.length == 2 && valor[1].Avaliar() != 0){
 			this.resultado = valor[0].Avaliar() / valor[1].Avaliar();
 			System.out.println("Operandos: " + valor[0].Avaliar() + " e " + valor[1].Avaliar() +"\n"
 					+ "Simbolo: " + simboloOperador + "\n" + "Resultado: " + this.resultado);
 		}
 		else{
-			try{
-				throw new NumeroOperandosException("Numero de operandos n„o suportado");
-			} catch(Exception e){
-				System.out.println("Capturada: " + e);
-				e.printStackTrace();
-			}
-			try{
-				throw new DivisaoPorZeroException("ImpossÌvel dividir por zero");
-			} catch(Exception b){
-				System.out.println("Capturada: " + b);
-				b.printStackTrace();
-			}
+			
+			throw new NumeroOperandosException("Numero de operandos n√£o suportado");
+			throw new DivisaoPorZeroException("Imposs√≠vel dividir por zero");			
 		}
 		return resultado;
 	}
